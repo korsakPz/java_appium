5640ffd98b6857731ecc88ff759b51dbdfb7e251
korsakPz <76098258+korsakPz@users.noreply.github.com>
2025-08-26 18:41:55 +0300

    Allure завис имости

--- testsAppium_maven/testsAppium/src/test/java/lib/CoreTestCase.java
+++ testsAppium_maven/testsAppium/src/test/java/lib/CoreTestCase.java
@@ -2,10 +2,16 @@ package lib;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Rule;
 import org.openqa.selenium.remote.RemoteWebDriver;
+import io.qameta.allure.junit4.AllureJunit4;
+import org.junit.rules.TestRule;
 
 
 public class CoreTestCase{
+
+    @Rule
+    public TestRule allureRule = (TestRule) new AllureJunit4();
 
     protected RemoteWebDriver driver;
     protected Platform Platform;
