78837e4208286b277fdc8d6bfcf512f5022881bd
korsakPz <76098258+korsakPz@users.noreply.github.com>
2025-08-26 13:55:07 +0300

    Добавление RemoteWebDriver

--- testsAppium_maven/testsAppium/src/test/java/lib/CoreTestCase.java
+++ testsAppium_maven/testsAppium/src/test/java/lib/CoreTestCase.java
@@ -3,25 +3,22 @@ package lib;
 import io.appium.java_client.android.AndroidDriver;
 import org.junit.After;
 import org.junit.Before;
-import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
-import java.net.URL;
 
 public class CoreTestCase{
 
-    protected AndroidDriver driver;
-
-    private static String AppiumURL = "http://127.0.0.1:4723";
-    String apkPlace = "C:\\Users\\Korsak\\Documents\\KURSES\\java_appium\\JavaAppium\\APKs\\org_wikipedia.apk";
-
-    private static final String PLATFORM_ANDROID = "android";
+    protected RemoteWebDriver driver;
+    protected Platform Platform;
 
 
     @Before
     public void setUp() throws Exception {
-      DesiredCapabilities capabilities = this.getCapabilitiesByPlatformEnv();
 
-        driver = new AndroidDriver(new URL(AppiumURL), capabilities);
+        this.Platform = new Platform();
+        driver = this.Platform.getDriver();
+
+
     }
 
     @After
@@ -31,26 +28,5 @@ public class CoreTestCase{
         }
     }
 
-    private DesiredCapabilities getCapabilitiesByPlatformEnv() throws Exception {
-        String platform = System.getenv("PLATFORM");
 
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-
-        if (platform == null) {
-            platform = "android"; // значение по умолчанию
-        }
-        if (platform.equals(PLATFORM_ANDROID)) {
-
-            capabilities.setCapability("platformName", "Android");
-            capabilities.setCapability("deviceName", "AndroidTestDevices");
-            capabilities.setCapability("platformVersion", "12.0");
-            capabilities.setCapability("appPackage", "org.wikipedia");
-            capabilities.setCapability("appActivity", ".main.MainActivity");
-            capabilities.setCapability("automationName", "UiAutomator2");
-            capabilities.setCapability("app", apkPlace);
-        } else {
-            throw new Exception("Cannot find platform. Now we have platform " + platform);
-        }
-        return capabilities;
-    }
 }
--- testsAppium_maven/testsAppium/src/test/java/lib/Platform.java
+++ testsAppium_maven/testsAppium/src/test/java/lib/Platform.java
@@ -1,24 +1,41 @@
 package lib;
 
 import io.appium.java_client.android.AndroidDriver;
-import org.junit.After;
-import org.junit.Before;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 import java.net.URL;
 
 public class Platform {
 
+    private static String APPIUM_URL = "http://127.0.0.1:4723";
+    String apkPlace = "C:\\Users\\Korsak\\Documents\\KURSES\\java_appium\\JavaAppium\\APKs\\org_wikipedia.apk";
+
+    private static final String PLATFORM_ANDROID = "android";
+    private static final String MOBILE_WEB_PLATFORM = "Mobile_web";
+
+    // Проверка и установка платформы АНДРОИД
+    public RemoteWebDriver getDriver() throws Exception {
+        URL URL = new URL(APPIUM_URL);
+
+        if (this.isAndroid()) {
+            return new AndroidDriver(URL, getAndroidDesiredCapabilities());
+        } else {
+            throw new Exception("Cannot need platform. Find another platform " + this.getPlatformVar());
+        }
+
+    }
+
+    public boolean isAndroid() {
+        return isPlatform(PLATFORM_ANDROID);
+    }
+
+    public boolean isMw() {
+        return isPlatform(MOBILE_WEB_PLATFORM);
+    }
 
 
-    protected AndroidDriver driver;
-    private static String AppiumURL = "http://127.0.0.1:4723";
-
-
-    @Before
-    public void setUp() throws Exception {
-
-        String apkPlace = "C:\\Users\\Korsak\\Documents\\KURSES\\java_appium\\JavaAppium\\APKs\\org_wikipedia.apk";
+    private DesiredCapabilities getAndroidDesiredCapabilities() {
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
 
@@ -30,14 +47,18 @@ public class Platform {
         capabilities.setCapability("automationName", "UiAutomator2");
         capabilities.setCapability("app", apkPlace);
 
+        return capabilities;
 
-        driver = new AndroidDriver(new URL(AppiumURL), capabilities);
     }
 
-    @After
-    public void tearDown() throws Exception{
-        if (driver != null) {
-            driver.quit();
-        }
+    private boolean isPlatform(String my_platform) {
+        String platform = this.getPlatformVar();
+        return my_platform.equals(platform);
     }
+
+    private String getPlatformVar() {
+        return System.getenv("PLATFORM");
+    }
+
+
 }
